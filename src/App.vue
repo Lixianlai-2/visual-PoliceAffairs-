149b77e0eae380ed8223038080b8e6fd49fe45a9
Li <1643565650@qq.com>
2022-09-06 17:40:22 +0800

    制作一个div，无论页面如何变化，还是占据固定的比例

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import Home from "./components/Home.vue";
 </script>
 
 <template>
   <div>你好</div>
+  <Home />
 </template>
 
 <style scoped></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="x"></div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped>
+@width: 367;
+@height: 315;
+
+.x {
+  width: (@width / 2420) * 100rem;
+  height: (@height / 2420) * 100rem;
+  border: 1px solid red;
+}
+</style>
